dde5e5a71cfe0f910b7b15adaa7c86aec0cb2bb9
bpaser <60895408+bpaser@users.noreply.github.com>
2022-01-26 13:47:37 -0600

    cleaned up

--- src/main/java/Models/Account.java
+++ src/main/java/Models/Account.java
@@ -2,41 +2,4 @@ package Models;
 
 public class Account {
 
-    private int id;
-    private Customer customer;
-    private double amount;
-
-    public Account(){
-
-    }
-
-    public Account(int id, Customer customer, double amount) {
-        this.id = id;
-        this.customer = customer;
-        this.amount = amount;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
 }
--- src/main/java/Models/Person.java
+++ src/main/java/Models/Person.java
@@ -1,13 +1,60 @@
 package Models;
 
-public abstract class Person {
+public class Person {
 
-    public int age;
-    public String name;
+    private int id;
+    private String name;
+    private String password;
+    private type pertype;
+
 
     public Person(){
-        this.age = 0;
         this.name = "";
+        this.id = 0;
+        this.password = "";
+        pertype = null;
     }
 
+    public Person(int id, String name, String password, type pertype) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.pertype = pertype;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPertype(type pertype) {
+        this.pertype = pertype;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public type getPertype() {
+        return pertype;
+    }
+
+    public void displayInfo(){
+        System.out.println(getId() + " " + getName() + " " + getPassword() + " " + getPertype());
+    }
 }
